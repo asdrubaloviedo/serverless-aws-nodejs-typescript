a79f22cc839dc85e145a7258cd628c10dbb41612
asdrubal <asdrubaloviedo@gmail.com>
2024-09-15 13:18:23 -0500

    Lambda additions

--- src/functions/obtenerPersona/handler.ts
+++ src/functions/obtenerPersona/handler.ts
@@ -1,7 +1,7 @@
 import { formatJSONResponse } from '../../libs/api-gateway';
 
 export const handler = (event: any) => {
-  console.log('Test Api function');
+  console.log('Test obtenerPersona function');
   return {
     message: 'Go Serverless v3.0! Your function executed successfully!',
     input: event
